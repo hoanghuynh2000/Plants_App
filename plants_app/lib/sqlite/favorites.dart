00433de60c0502dea1d61793be0fb60f8b0dc3f2
ASUS <huynh13092000@gmail.com>
2021-10-12 18:23:04 +0700

    update favoritess

--- plants_app/lib/sqlite/favorites.dart
+++ plants_app/lib/sqlite/favorites.dart
@@ -1,22 +1,29 @@
+import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:plants_app/model/mdfavorites.dart';
 
-class FavoritesDatabase {
-  static final FavoritesDatabase instance = FavoritesDatabase._init();
+class FavoritesDatabase  {
+    static final FavoritesDatabase instance = FavoritesDatabase._init();
+  String id='id';
+  String isImportant='isImportant';
+  String idProduct='idProduct';
+  String productName='productName';
+  String categoryName='categoryName';
+  String price='price';
+  String images='images';
 
   static Database? _database;
 
   FavoritesDatabase._init();
-
   Future<Database> get database async {
     if (_database != null) return _database!;
 
-    _database = await _initDB('favorites.db');
+    _database = await _initDB('tablefavorites1.db');
     return _database!;
   }
 
-  get tableFavorites => null;
+  String tableFavorites='tablefavorites1';
 
   Future<Database> _initDB(String filePath) async {
     final dbPath = await getDatabasesPath();
@@ -24,95 +31,75 @@ class FavoritesDatabase {
 
     return await openDatabase(path, version: 1, onCreate: _createDB);
   }
-
   Future _createDB(Database db, int version) async {
-    final idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    final idType = 'TEXT PRIMARY KEY';
     final textType = 'TEXT NOT NULL';
     final boolType = 'BOOLEAN NOT NULL';
     final integerType = 'INTEGER NOT NULL';
 
-    await db.execute('''
-CREATE TABLE ${tableFavorites} ( 
-  ${FavoritesFields.id} $idType, 
-  ${FavoritesFields.isImportant} $boolType,
-  ${FavoritesFields.idProduct} $textType,
-  ${FavoritesFields.productName} $textType,
-  ${FavoritesFields.categoryName} $textType,
-  ${FavoritesFields.price} $integerType,
-  ${FavoritesFields.images} $textType
-  )
-''');
+    await db.execute('CREATE TABLE tablefavorites1 ($id $idType , $isImportant $integerType, $idProduct $textType, $productName $textType, $categoryName $textType, $price $integerType, $images $textType)');
   }
-
-  Future<Favorites> create(Favorites favorites) async {
-    final db = await instance.database;
-
-    // final json = favorites.toJson();
-    // final columns =
-    //     '${FavoritesFields.title}, ${FavoritesFields.description}, ${FavoritesFields.time}';
-    // final values =
-    //     '${json[FavoritesFields.title]}, ${json[FavoritesFields.description]}, ${json[FavoritesFields.time]}';
-    // final id = await db
-    //     .rawInsert('INSERT INTO table_name ($columns) VALUES ($values)');
-
-    final id = await db.insert(tableFavorites, favorites.toJson());
-    return favorites.copy(id: id);
-  }
-
-  Future<Favorites> readFavorites(int id) async {
-    final db = await instance.database;
-
-    final maps = await db.query(
-      tableFavorites,
-      columns: FavoritesFields.values,
-      where: '${FavoritesFields.id} = ?',
-      whereArgs: [id],
+   Future<void> insertFav(Favorites favorites) async {
+    // Get a reference to the database.
+    final db = await database;
+    await db.insert(
+      'tablefavorites1',
+      favorites.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
     );
+  }
+   Future<List<Favorites>> getFavById(String? id) async {
+      final db = await database; 
+      List<Map<String, dynamic>> maps = await db.query("tablefavorites1", where: 'idProduct = ?', whereArgs: [id]); 
+      return List.generate(maps.length, (i) {
+      return Favorites(
+        id: maps[i]['id'], 
+        isImportant:  maps[i]['isImportant'], 
+        idProduct:  maps[i]['idProduct'], 
+        productName:  maps[i]['productName'], 
+        categoryName:  maps[i]['categoryName'], 
+        price:  maps[i]['price'], 
+        images:  maps[i]['images']);
+    });
+   } 
+ Future<List<Favorites>> readfavorites() async {
+    final db = await database;
 
-    if (maps.isEmpty) {
-      return Favorites.fromJson(maps.first);
-    } else {
-      throw Exception('ID $id not found');
-    }
+    // Query the table for all The Dogs.
+    final List<Map<String, dynamic>> maps = await db.query('tablefavorites1');
+
+    return List.generate(maps.length, (i) {
+      return Favorites(
+        id: maps[i]['id'], 
+        isImportant:  maps[i]['isImportant'], 
+        idProduct:  maps[i]['idProduct'], 
+        productName:  maps[i]['productName'], 
+        categoryName:  maps[i]['categoryName'], 
+        price:  maps[i]['price'], 
+        images:  maps[i]['images']);
+    });
   }
 
-  Future<List<Favorites>> readAllFavorites() async {
-    final db = await instance.database;
-
-    final orderBy = '${FavoritesFields.time} ASC';
-    // final result =
-    //     await db.rawQuery('SELECT * FROM $tableFavorites ORDER BY $orderBy');
-
-    final result = await db.query(tableFavorites, orderBy: orderBy);
-
-    return result.map((json) => Favorites.fromJson(json)).toList();
-  }
-
-  Future<int> update(Favorites favorites) async {
-    final db = await instance.database;
-
-    return db.update(
-      tableFavorites,
-      favorites.toJson(),
-      where: '${FavoritesFields.id} = ?',
+  Future<void> updateFav(Favorites favorites) async {
+    final db = await database;
+    await db.update(
+      'tablefavorites1',
+      favorites.toMap(),
+      where: 'id = ?',
       whereArgs: [favorites.id],
     );
   }
 
-  Future<int> delete(int id) async {
-    final db = await instance.database;
-
-    return await db.delete(
-      tableFavorites,
-      where: '${FavoritesFields.id} = ?',
+  Future<void> deleteFav(String? id) async {
+    final db = await database;
+    await db.delete(
+      'tablefavorites1',
+      where: 'idProduct = ?',
       whereArgs: [id],
     );
   }
-
-  Future close() async {
-    final db = await instance.database;
-
-    db.close();
+  Future close() async{
+    final db=await instance.database;
+    return db.close();
   }
 }
-
