03cfd7002a851c8cddb417b757548e7568fee7c4
ASUS <huynh13092000@gmail.com>
2021-10-12 17:05:48 +0700

    update

--- plants_app/lib/main.dart
+++ plants_app/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:plants_app/screens/detailsproduct.dart';
-import 'package:plants_app/screens/home.dart';
-import 'package:plants_app/screens/login.dart';
+import 'package:plants_app/screens/favorites.dart';
 
 void main() {
+  
   runApp(MaterialApp(
     home:DetailsProduct()
 
